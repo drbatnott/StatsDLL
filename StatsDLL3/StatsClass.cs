254da3f30b034110c1e302912c50a556a31ecfa0
Peter Blanchfield <zygomy@gmail.com>
2021-10-15 13:33:53 +0100

    Added the DLL project Created the Class and named it Added the Mean, mean and total properties and fields and the constructor

---
+++ StatsDLL3/StatsClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatsDLL3
+{
+    public class StatsClass
+    {
+        List<float> myList;
+        private float total;
+        private float mean;
+        public float Mean
+        {
+            get
+            {
+                if(myList.Count > 0)
+                {
+                    mean = total / myList.Count;
+                }
+                else
+                {
+                    total = 0;
+                    mean = 0;
+                }
+                return mean;
+            }
+        }
+        public StatsClass(float v1)
+        {
+            myList = new List<float>();
+            total = v1;
+            myList.Add(v1);
+        }
+    }
+}
